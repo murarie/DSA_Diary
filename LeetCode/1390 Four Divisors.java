dcc1ce25e4e7484fe19954b58188db101e68ba35
murarie <murarimishra1611@gmail.com>
2026-01-05 14:20:19 +0530

    add lc potd 1795

--- LeetCode/1390 Four Divisors.java
+++ LeetCode/1390 Four Divisors.java
@@ -33,3 +33,37 @@ Constraints:
 1 <= nums[i] <= 105 */
 
 
+//solution
+
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int total_sum = 0;
+        
+        for (int num : nums) {
+            int sum = sum_if_four(num);
+            total_sum += sum;
+        }
+        
+        return total_sum;
+    }
+
+    private int sum_if_four(int n) {
+        int d1 = 0, d2 = 0;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (d1 == 0) {
+                    d1 = i;
+                    d2 = n / i;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        if (d1 == 0) return 0;
+        if (d1 == d2) return 0;
+
+        return 1 + n + d1 + d2;
+    }
+}
