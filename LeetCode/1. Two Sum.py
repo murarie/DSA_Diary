81a2bf9f9efb56b5ab6b9732a058ee4cd7fd6392
murarie <murarimishra1611@gmail.com>
2026-01-06 12:02:30 +0530

    add lc potd 1161

--- LeetCode/1. Two Sum.py
+++ LeetCode/1. Two Sum.py
@@ -42,3 +42,11 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comple
 
 #solution
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+        return []   
