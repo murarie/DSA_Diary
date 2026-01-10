ab0a1b3e81a9decba1c77215768af9306aae778e
murarie <murarimishra1611@gmail.com>
2026-01-10 21:56:05 +0530

    add lc potd 712

--- LeetCode/865. Smallest Subtree with all the Deepest Nodes.java
+++ LeetCode/865. Smallest Subtree with all the Deepest Nodes.java
@@ -39,11 +39,56 @@ Constraints:
 The number of nodes in the tree will be in the range [1, 500].
 0 <= Node.val <= 500
 The values of the nodes in the tree are unique.
- 
+
 
 Note: This question is the same as 1123: https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
 */
 
 //solution
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Pair {
+    int depth;
+    TreeNode node;
+    
+    Pair(int d, TreeNode n) {
+        depth = d;
+        node = n;
+    }
+}
 
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+    private Pair dfs(TreeNode root) {
+        if(root == null) {
+            return new Pair(0, null);
+        }
+
+        Pair l = dfs(root.left);
+        Pair r = dfs(root.right);
+
+        if(l.depth > r.depth) {
+            return new Pair(l.depth + 1, l.node);
+        }
+        if(r.depth > l.depth) {
+            return new Pair(r.depth + 1, r.node);
+        }
+        return new Pair(l.depth + 1 , root);
+    }
+}
