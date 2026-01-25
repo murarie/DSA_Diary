63416d2b86d241f2c6c127d2fac5775772e408b0
murarie <murarimishra1611@gmail.com>
2026-01-25 22:43:45 +0530

    add lc potd 1984

--- LeetCode/21. Merge Two Sorted Lists.cpp
+++ LeetCode/21. Merge Two Sorted Lists.cpp
@@ -36,3 +36,38 @@ Both list1 and list2 are sorted in non-decreasing order.*/
 
 // solution
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* result = dummy;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val < list2->val) {
+                result->next = list1;
+                result = list1;
+                list1 = list1->next;
+            } else {
+                result->next = list2;
+                result = list2;
+                list2 = list2->next;
+            }
+        }
+        result->next = NULL;
+        if (list1)
+            result->next = list1;
+        else
+            result->next = list2;
+
+        return dummy->next;
+    }
+};
