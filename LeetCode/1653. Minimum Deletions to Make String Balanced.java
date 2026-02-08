166fb5036fc58873e8a147581778a6fda011cfb6
murarie <murarimishra1611@gmail.com>
2026-02-08 23:37:55 +0530

    add lc potd 110

--- LeetCode/1653. Minimum Deletions to Make String Balanced.java
+++ LeetCode/1653. Minimum Deletions to Make String Balanced.java
@@ -37,3 +37,18 @@ s[i] is 'a' or 'b'​​.
 
 //Solution
 
+class Solution {
+    public int minimumDeletions(String s) {
+        int countB = 0;
+        int deletions = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == 'b') {
+                countB++;
+            } else {
+                deletions = Math.min(deletions + 1, countB);
+            }
+        }
+        return deletions;
+    }
+}
