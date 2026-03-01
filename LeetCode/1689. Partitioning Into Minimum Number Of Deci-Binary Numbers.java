acbe48a438784e533fa1c5c7e9f6eaa043350967
murarie <murarimishra1611@gmail.com>
2026-03-01 19:46:13 +0530

    lc potd 1689

---
+++ LeetCode/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -0,0 +1,16 @@
+/*
+ */
+
+// Solution
+
+class Solution {
+    public int minPartitions(String n) {
+        for (char ch = '9'; ch >= '0'; ch--) {
+            if (n.indexOf(ch) != -1) {
+                return ch - '0';
+            }
+        }
+
+        return -1;
+    }
+}
