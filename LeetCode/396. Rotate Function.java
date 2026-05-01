913e2ab9c94cca5b418ed5fdc416e3392282d203
murarie <murarimishra1611@gmail.com>
2026-05-01 16:32:03 +0530

    lc potd 396

---
+++ LeetCode/396. Rotate Function.java
@@ -0,0 +1,26 @@
+/*
+ */
+
+// Solution
+
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        int curr = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            curr += i * nums[i];
+        }
+
+        int ans = curr;
+
+        for (int i = 1; i < n; i++) {
+            curr = curr - sum + n * nums[i - 1];
+            ans = Math.max(ans, curr);
+        }
+
+        return ans;
+    }
+}
