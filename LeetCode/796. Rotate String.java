2aa2c561d8f9ae477f313b651fcadff75bfb4429
murarie <murarimishra1611@gmail.com>
2026-05-03 22:31:48 +0530

    lc potd 796

---
+++ LeetCode/796. Rotate String.java
@@ -0,0 +1,11 @@
+/*
+ */
+
+// Solution
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())return false;
+        return (s+s).contains(goal);
+    }
+}
