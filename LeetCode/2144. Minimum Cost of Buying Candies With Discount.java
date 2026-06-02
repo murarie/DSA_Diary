b34b1d0d2e6f72019d1847fd27d82a9b0111eae7
murarie <murarimishra1611@gmail.com>
2026-06-02 23:11:44 +0530

    lc potd 3633

--- LeetCode/2144. Minimum Cost of Buying Candies With Discount.java
+++ LeetCode/2144. Minimum Cost of Buying Candies With Discount.java
@@ -1,5 +1,37 @@
 /*
- */
+import java.util.Arrays;
+
+class Solution {
+    public int minimumCost(int[] cost) {
+        int totalCost = 0;
+        
+        Arrays.sort(cost);
+
+        int l = cost.length;
+        int numThree = l / 3;
+        int modThree = l % 3;
+
+        int pos = l - 1;
+
+        for (int i = 0; i < numThree; i++) {
+            int firstCandy = cost[pos];
+            totalCost = totalCost + firstCandy;
+            pos--;
+            
+            int secondCandy = cost[pos];
+            totalCost = totalCost + secondCandy;
+            pos -= 2; 
+        }
+
+        for (int i = 0; i < modThree; i++) {
+            int candy = cost[pos];
+            totalCost = totalCost + candy;
+            pos--;
+        }
+
+        return totalCost;        
+    }
+} */
 
 // Solution
 
