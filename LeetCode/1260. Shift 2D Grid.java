c0063bfb0de335b493396956f81a404850d8aa6a
murarie <murarimishra1611@gmail.com>
2026-07-21 19:45:22 +0530

    lc potd 3499

--- LeetCode/1260. Shift 2D Grid.java
+++ LeetCode/1260. Shift 2D Grid.java
@@ -1,5 +1,38 @@
 /*
- */
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int row = grid.length;
+        int col = grid[0].length;
+
+        int[][] rotatedGrid = new int[row][col];
+        List<List<Integer>> rotatedList = new ArrayList<>();
+
+        int rotation = k % (row * col);
+        rotation = rotation > 0 ? rotation : row * col + rotation;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+                int currCol = (j + rotation) % col;
+                int currRow = (i + (j + rotation) / col) % row;
+
+                rotatedGrid[currRow][currCol] = grid[i][j];
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            List<Integer> rowList = new ArrayList<>();
+
+            for (int j = 0; j < col; j++) {
+                rowList.add(rotatedGrid[i][j]);
+            }
+
+            rotatedList.add(rowList);
+        }
+
+        return rotatedList;
+    }
+} */
 
 // Solution
 
