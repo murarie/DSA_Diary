f519606194aa6a86e3569ee67d342d4d5d477cb9
murarie <murarimishra1611@gmail.com>
2026-08-13 22:51:53 +0530

    lc potd 1727

--- LeetCode/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum.java
+++ LeetCode/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum.java
@@ -34,3 +34,26 @@ Constraints:
 // Solution
 
 
+class Solution {
+    public int missingInteger(int[] nums) {
+        int n=nums.length;
+        int sum=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]+1){
+                sum+=nums[i];
+            }
+            else break;
+        }
+
+         HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
+        }
+
+        while (set.contains(sum)) {
+            sum++;
+        }
+        return sum;
+        
+    }
+}
